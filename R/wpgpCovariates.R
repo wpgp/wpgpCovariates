bd4932786e93ad09dc42411079ef75780de1fa4a
worldpopglobal <31058264+worldpopglobal@users.noreply.github.com>
2017-08-18 00:30:14 +0100

    Add files via upload
    to wpgpListCountryCovariates a new param was added "frCSVDownload" If TRUE, a new wpgAllCovariates.csv file will downloaded

--- R/wpgpCovariates.R
+++ R/wpgpCovariates.R
@@ -1,3 +1,28 @@
+# Function to get time difference in human readable format
+# Input is start time and end time
+# If "frm" is set to "hms" then output will be h:m:s
+# otherwise only hours will be returned
+tmDiff <- function(start, end, frm="hms") {
+
+  dsec <- as.numeric(difftime(end, start, units = c("secs")))
+  hours <- floor(dsec / 3600)
+
+  if (frm == "hms" ){
+    minutes <- floor((dsec - 3600 * hours) / 60)
+    seconds <- dsec - 3600*hours - 60*minutes
+
+    out=paste0(
+      sapply(c(hours, minutes, seconds), function(x) {
+        formatC(x, width = 2, format = "d", flag = "0")
+      }), collapse = ":")
+
+    return(out)
+  }else{
+    return(hours)
+  }
+}
+
+
 # Function to download file from ftp server
 #
 # @param file_path is a path to a remoute file
@@ -5,17 +30,22 @@
 # @param username ftp username to WorldPop ftp server
 # @param password ftp password to WorldPop ftp server
 # @param quiet If TRUE, suppress status messages (if any), and the progress bar.
+# @param method Method to be used for downloading files.
+#  Current download methods are "internal", "wininet" (Windows only) "libcurl",
+# "wget" and "curl", and there is a value "auto"
 # @rdname wpgpDownloadFileFromFTP
 #' @importFrom utils read.csv
-wpgpDownloadFileFromFTP <- function(file_path, dest_file, username, password, quiet) {
+wpgpDownloadFileFromFTP <- function(file_path, dest_file, username, password, quiet, method="auto") {
 
   wpgpFTP <- "ftp.worldpop.org.uk"
   credentials <- paste(username, password, sep = ":")
   file_remote <-paste0('ftp://',credentials,'@',wpgpFTP,'/WP515640_Global/Covariates/',file_path)
 
+  tmStartDw  <- Sys.time()
+
   tryCatch(
     {
-      utils::download.file(file_remote, destfile=dest_file,mode="wb",quiet=quiet)
+      utils::download.file(file_remote, destfile=dest_file,mode="wb",quiet=quiet, method=method)
     },
     error=function(cond) {
       message(paste("URL does not seem to exist:", file_remote))
@@ -30,8 +60,10 @@ wpgpDownloadFileFromFTP <- function(file_path, dest_file, username, password, qu
       return(NULL)
     },
     finally={
-      if (quiet){
-        message(paste("Processed URL:", file_remote))
+      if (!quiet){
+        tmEndDw  <- Sys.time()
+        #message(paste("Processed URL:", file_remote))
+        message(paste("It took ", tmDiff(tmStartDw ,tmEndDw,frm="hms"), "to download" ))
       }
     }
   )
@@ -47,10 +79,12 @@ wpgpDownloadFileFromFTP <- function(file_path, dest_file, username, password, qu
 # @param username ftp username to WorldPop ftp server
 # @param password ftp password to WorldPop ftp server
 # @param quiet If TRUE, suppress status messages (if any), and the progress bar.
+# @param frCSVDownload If TRUE, a new wpgAllCovariates.csv file will
+# be downloaded and old one removed.
 # @rdname wpgpGetCSVFileAllCovariates
 # @return the file will be downloaded.
 #' @importFrom utils read.csv
-wpgpGetCSVFileAllCovariates <- function(username, password) {
+wpgpGetCSVFileAllCovariates <- function(username, password, frCSVDownload=FALSE) {
 
 #  wpgpAllCSVFilesPath <- tempfile(pattern = "wpgpAllCovariates",
 #                                  tmpdir = tempdir(),
@@ -58,7 +92,7 @@ wpgpGetCSVFileAllCovariates <- function(username, password) {
 
   wpgpAllCSVFilesPath <- paste0(tempdir(),"/wpgpAllCovariates.csv")
 
-  if(!file.exists(wpgpAllCSVFilesPath)){
+  if(!file.exists(wpgpAllCSVFilesPath) | frCSVDownload){
 
     credentials <- paste(username,password,sep = ":")
     file_remote <-paste0('wpgAllCovariates.csv')
@@ -85,12 +119,13 @@ wpgpGetCSVFileAllCovariates <- function(username, password) {
 #' @param username ftp username to WorldPop ftp server
 #' @param password ftp password to WorldPop ftp server
 #' @param verbose quiet If TRUE, suppress status messages (if any)
+#' @param frCSVDownload If TRUE, a new wpgAllCovariates.csv file will downloaded
 #' @rdname wpgpListCountries
 #' @return Dataframe
 #' @export
-wpgpListCountries <- function(username, password, verbose=FALSE) {
+wpgpListCountries <- function(username, password, verbose=FALSE, frCSVDownload=FALSE) {
 
-  df <- wpgpGetCSVFileAllCovariates(username, password)
+  df <- wpgpGetCSVFileAllCovariates(username, password, frCSVDownload)
 
   df.sub<-as.data.frame(df[!duplicated(df$ISO3), ])
 
@@ -121,12 +156,17 @@ wpgpListCountries <- function(username, password, verbose=FALSE) {
 #' @param username ftp username to WorldPop ftp server
 #' @param password ftp password to WorldPop ftp server
 #' @param detailed If TRUE, then more information will be given
+#' @param frCSVDownload If TRUE, a new wpgAllCovariates.csv file will downloaded
 #' @rdname wpgpListCountryCovariates
 #' @return Dataframe
 #' @export
 #' @examples
 #' wpgpListCountryCovariates( ISO3="USA", username="ftpUsername", password="ftpPassword" )
-wpgpListCountryCovariates <- function(ISO3=NULL, username=NULL, password=NULL, detailed=FALSE) {
+wpgpListCountryCovariates <- function(ISO3=NULL,
+                                      username=NULL,
+                                      password=NULL,
+                                      detailed=FALSE,
+                                      frCSVDownload=FALSE) {
 
   if (is.null(ISO3))  stop("Enter country ISO3" )
   if (is.null(username)) stop("Enter ftp username" )
@@ -139,7 +179,7 @@ wpgpListCountryCovariates <- function(ISO3=NULL, username=NULL, password=NULL, d
     stop( paste0("country code should be three letters. You entered : ",uISO3," length is ",length_country))
   }
 
-  df <- wpgpGetCSVFileAllCovariates(username, password)
+  df <- wpgpGetCSVFileAllCovariates(username, password, frCSVDownload)
 
   if (!any(df$ISO3==uISO3))
     stop( paste0("Entered ISO3: ",uISO3," does not present in WP"))
@@ -172,6 +212,10 @@ wpgpListCountryCovariates <- function(ISO3=NULL, username=NULL, password=NULL, d
 #' @param username ftp username to WorldPop ftp server
 #' @param password ftp password to WorldPop ftp server
 #' @param quiet Download Without any messages if TRUE
+#' @param frCSVDownload If TRUE, a new wpgAllCovariates.csv file will downloaded
+#' @param method Method to be used for downloading files. Current download methods
+#' are "internal", "wininet" (Windows only) "libcurl", "wget" and
+#' "curl", and there is a value "auto"
 #' @rdname wpgpGetCountryCovariate
 #' @return Raster file will be downloaded
 #' @export
@@ -183,7 +227,9 @@ wpgpGetCountryCovariate <- function(ISO3=NULL,
                                      destDir=NULL,
                                      username=NULL,
                                      password=NULL,
-                                     quiet=FALSE) {
+                                     quiet=TRUE,
+                                     frCSVDownload=FALSE,
+                                     method="auto") {
 
 
   if (!dir.exists(destDir)) stop( paste0("Please check existens destDir: ",destDir))
@@ -193,7 +239,7 @@ wpgpGetCountryCovariate <- function(ISO3=NULL,
   if (is.null(username)) stop("Error: Enter ftp username" )
   if (is.null(password)) stop("Error: Enter ftp password" )
 
-  df <- wpgpGetCSVFileAllCovariates(username, password)
+  df <- wpgpGetCSVFileAllCovariates(username, password, frCSVDownload)
 
   ISO3 <- toupper(ISO3)
   covariate <- tolower(covariate)
@@ -201,7 +247,7 @@ wpgpGetCountryCovariate <- function(ISO3=NULL,
   df.filtered <- df[df$ISO3 == ISO3 & df$CvtName == covariate & df$Year == year, ]
 
   if (nrow(df.filtered)!=1){
-    stop( paste0("Entered Covariates: ",covariate," does not present in WP"))
+    stop( paste0("Entered Covariates: ",covariate," does not present in WP. Please check Year and name of the dataset"))
   }
 
   credentials <- paste(username,password,sep = ":")
@@ -210,7 +256,7 @@ wpgpGetCountryCovariate <- function(ISO3=NULL,
 
   file_local <- paste0(destDir,'/',df.filtered$RstName,'.tif')
 
-  wpgpDownloadFileFromFTP(file_remote, file_local, username, password, quiet=quiet)
+  wpgpDownloadFileFromFTP(file_remote, file_local, username, password, quiet=quiet, method=method)
 
 
 }
